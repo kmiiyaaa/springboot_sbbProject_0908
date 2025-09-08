64ed379158a2c6135f6af586aff75bef287e57d6
kmiiyaaa <ckm1533@naver.com>
2025-09-08 12:52:26 +0900

    게시판 질문글 상세내용보기

--- src/main/java/com/kmii/kmiboard/question/QuestionController.java
+++ src/main/java/com/kmii/kmiboard/question/QuestionController.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class QuestionController {
 	
 	@Autowired
 	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private QuestionService questionService;
+	
 	
 	@GetMapping(value="/") //root 요청 처리
 	public String root() {
@@ -23,11 +29,21 @@ public class QuestionController {
 	// @ResponseBody  ->  return 옆에 적어준 문자열 그대로 화면에 출력
 	public String list(Model model) {
 		
-		List<Question> questionList = questionRepository.findAll(); // 모든 질문글 불러오기
+		// List<Question> questionList = questionRepository.findAll(); // 모든 질문글 불러오기
+		List<Question> questionList = questionService.getList();
 		model.addAttribute("questionList", questionList);
 		
 		
 		return "question_list";
+	}
+	
+	@GetMapping(value="/question/detail/{id}")  // 파라미터이름 없이 값만 넘어 왔을때 처리
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		
+		//service에 4(질문글 번호) 넣어서 호출
+		Question question = questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail"; // 타임리프 html의 이름
 	}
 	
 
--- src/main/java/com/kmii/kmiboard/question/QuestionService.java
+++ src/main/java/com/kmii/kmiboard/question/QuestionService.java
@@ -1,10 +1,36 @@
 package com.kmii.kmiboard.question;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class QuestionService {
 	
+	//@Autowired
+	private final QuestionRepository questionRepository;  
+	//@RequiredArgsConstructor 이용해서 생성자 방식으로 주입된 questionRepository - final 필드만 가능
+	
+	public List<Question> getList() {//모든 질문글 가져오기
+		return questionRepository.findAll();
+		
+	}
+	
+	public Question getQuestion(Integer id) {
+		Optional<Question> qOptional = questionRepository.findById(id);
+		
+		if(qOptional.isPresent()) {
+			return qOptional.get();  // question 반환
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
+		
+	}
 	
 
 }
