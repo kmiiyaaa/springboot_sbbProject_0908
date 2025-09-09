b3be891cea13b6a90381adc51c5e44a3733268c1
kmiiyaaa <ckm1533@naver.com>
2025-09-09 11:10:51 +0900

    테스트 데이터 300개 생성 테스트케이스

---
+++ src/test/java/com/kmii/kmiboard/DummyDataInputTest.java
@@ -0,0 +1,26 @@
+package com.kmii.kmiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kmii.kmiboard.question.QuestionService;
+
+@SpringBootTest
+public class DummyDataInputTest {
+	
+	@Autowired
+	private QuestionService questionService;
+	
+	@Test
+	public void testDummy() {
+		
+		for(int i=1;i<=300;i++) {
+			String subject = String.format("테스트 데이터 입니다:[%03d]", i);
+			String content = "연습 내용 테스트 데이터 입니다.";
+			questionService.create(subject,content);
+		
+		}
+	}
+	
+}
